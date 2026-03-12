763a7c5b258fe2fde1d78bc5bdcad053f7b647d4
Abirami1021 <abiashwinps@gmail.com>
2026-03-12 13:20:15 +0530

    Updated code

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,10 +1,13 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import utilities.ScreenshotUtil;
+import org.testng.ITestResult;
 
 public class BaseTest {
 
@@ -21,7 +24,15 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+
+        if(result.getStatus() == ITestResult.SUCCESS) {
+            ScreenshotUtil.captureScreenshot(driver, result.getName()+"_PASS");
+        }
+
+        if(result.getStatus() == ITestResult.FAILURE) {
+            ScreenshotUtil.captureScreenshot(driver, result.getName()+"_FAIL");
+        }
 
         if(driver != null) {
             driver.quit();
---
+++ src/test/java/utilities/ScreenshotUtil.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import org.apache.commons.io.FileUtils;
+
+public class ScreenshotUtil {
+
+    public static void captureScreenshot(WebDriver driver, String testName) {
+
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(src, new File("screenshots/" + testName + ".png"));
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
