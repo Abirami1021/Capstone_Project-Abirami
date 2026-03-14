24590885374d57135a01ec8ed6e7c57af675cc1c
Abirami1021 <abiashwinps@gmail.com>
2026-03-14 20:12:37 +0530

    Changed assertions

--- src/test/java/tests/FlightBookingTest.java
+++ src/test/java/tests/FlightBookingTest.java
@@ -3,7 +3,6 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.openqa.selenium.By;
 
 import base.BaseTest;
 import pages.HomePage;
@@ -12,14 +11,14 @@ import pages.PurchasePage;
 
 public class FlightBookingTest extends BaseTest {
 
-	@DataProvider(name="bookingData")
-	public Object[][] getData(){
-	    return new Object[][]{
-	     {"Paris","London","Abirami","106/2 Coimbatore","Coimbatore","TN","627814","9876543210","3","2026","Abirami"},
-	     {"Boston","Berlin","Kavi","12 Street","Chennai","TN","75001","1234567890","5","2027","Kavi"},
-	     {"San Diego","New York","","45 Road","Bangalore","KA","560001","9871234567","6","2028","Pravee"}
-	    };
-	}
+    @DataProvider(name="bookingData")
+    public Object[][] getData(){
+        return new Object[][]{
+         {"Paris","London","Abirami","106/2 Coimbatore","Coimbatore","TN","627814","9876543210","3","2026","Abirami"},
+         {"Boston","Berlin","Kavi","12 Street","Chennai","TN","75001","1234567890","5","2027","Kavi"},
+         {"San Diego","New York","Pravee","45 Road","Bangalore","KA","560001","9871234567","6","2028","Pravee"}
+        };
+    }
 
     @Test(dataProvider="bookingData")
     public void bookFlightTest(String fromCity,String toCity,
@@ -40,6 +39,6 @@ public class FlightBookingTest extends BaseTest {
 
         Assert.assertTrue(str.contains("Thank you for your purchase"));
 
-        System.out.println("Flight booking completed");
+        System.out.println("Congratulations!,Your Flight Booked Successfully.");
     }
 }
